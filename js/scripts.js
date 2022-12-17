fcf8e82e1d4d52037fd9a13df1bb861d265d53d1
Zach Gallman <zach.ecab@gmail.com>
2022-12-17 11:27:53 -0500

    Week 4 assignment

--- js/scripts.js
+++ js/scripts.js
@@ -12,3 +12,12 @@ $(function () {
         }
     });
 });
+
+$(function () {
+    $("#reserveButton").click(function () {
+        $("#reserveModal").modal('show');
+    })
+    $("#loginButton").click(function () {
+        $("#loginModal").modal("show");
+    })
+})
